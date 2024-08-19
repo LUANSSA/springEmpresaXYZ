568661f298233e2da79e433d0696dcd2d7122796
Luan Souza <alunoluansenai@gmail.com>
2024-08-19 08:54:03 -0300

    ✨ Novo recurso: Adição de campo status do pedido e data de registro do pedido no Model e no Dto

--- src/main/java/com/example/springEmpresaXYZ/dtos/OrderRecordDto.java
+++ src/main/java/com/example/springEmpresaXYZ/dtos/OrderRecordDto.java
@@ -4,5 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
+public record OrderRecordDto(@NotBlank String orderStatus, @NotBlank Date registrationDate) {
 }
--- src/main/java/com/example/springEmpresaXYZ/models/OrderModel.java
+++ src/main/java/com/example/springEmpresaXYZ/models/OrderModel.java
@@ -21,15 +21,20 @@ public class OrderModel extends RepresentationModel<OrderModel> implements Seria
     @GeneratedValue(strategy=GenerationType.AUTO)
     private UUID idOrder;
     @NotNull
+    private String orderStatus;
+    private Date registrationDate;
 
-    public UUID getIdOrder() {
-        return idOrder;
-    }
+    public UUID getIdOrder() { return idOrder; }
 
-    public void setIdOrder(UUID idOrder) {
-        this.idOrder = idOrder;
-    }
+    public void setIdOrder(UUID idOrder) { this.idOrder = idOrder; }
 
+    public @NotNull String getOrderStatus() { return orderStatus; }
+
+    public void setOrderStatus(@NotNull String orderStatus) { this.orderStatus = orderStatus; }
+
+    public @NotNull Date getRegistrationDate() { return registrationDate; }
+
+    public void setRegistrationDate(@NotNull Date registrationDate) { this.registrationDate = registrationDate; }
 
 
 }
