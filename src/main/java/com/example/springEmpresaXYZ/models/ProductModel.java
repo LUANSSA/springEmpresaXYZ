d99323c630d1bb7d5e6390ab7b5b2fdc89a1016d
Luan Souza <alunoluansenai@gmail.com>
2024-08-19 08:35:42 -0300

    ✨ Novo recurso: Adição de campo descrição no Model de produto

--- src/main/java/com/example/springEmpresaXYZ/models/ProductModel.java
+++ src/main/java/com/example/springEmpresaXYZ/models/ProductModel.java
@@ -20,28 +20,21 @@ public class ProductModel extends RepresentationModel<ProductModel> implements S
 	private UUID idProduct;
 	private String name;
 	private BigDecimal value;
+	private String description;
 
-	public UUID getIdProduct() {
-		return idProduct;
-	}
+	public UUID getIdProduct() { return idProduct; }
 
-	public void setIdProduct(UUID idProduct) {
-		this.idProduct = idProduct;
-	}
+	public void setIdProduct(UUID idProduct) { this.idProduct = idProduct; }
 
-	public String getName() {
-		return name;
-	}
+	public String getName() { return name; }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+	public void setName(String name) { this.name = name; }
 
-	public BigDecimal getValue() {
-		return value;
-	}
+	public BigDecimal getValue() { return value; }
 
-	public void setValue(BigDecimal value) {
-		this.value = value;
-	}
+	public void setValue(BigDecimal value) { this.value = value; }
+
+	public String getDescription() { return description; }
+
+	public void setDescription(String description) { this.description = description; }
 }
