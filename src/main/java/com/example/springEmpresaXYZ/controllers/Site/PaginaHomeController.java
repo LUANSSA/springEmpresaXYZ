511cc25dcc0d74556f27fb5ef1b54ded09d67df1
Luan Souza <alunoluansenai@gmail.com>
2024-08-25 23:15:00 -0300

    ✨ Novo recurso: Controller de página home

---
+++ src/main/java/com/example/springEmpresaXYZ/controllers/Site/PaginaHomeController.java
@@ -0,0 +1,28 @@
+package com.example.springEmpresaXYZ.controllers.Site;
+
+import com.example.springEmpresaXYZ.controllers.ProductController;
+import com.example.springEmpresaXYZ.models.ProductModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PaginaHomeController {
+
+    private final ProductController productController;
+
+    public PaginaHomeController(ProductController productController) {
+        this.productController = productController;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        List<ProductModel> products = productController.getAllProducts().getBody();
+        model.addAttribute("products", products);
+        model.addAttribute("mensagem", "Olá Mundo");
+        return "index";
+    }
+}
