b0592e034fe1a14d3515fde383c97a73612e936c
Luan Souza <alunoluansenai@gmail.com>
2024-08-26 00:07:11 -0300

    ♻️ Refatoração: Record DTO Product

--- src/main/java/com/example/springEmpresaXYZ/dtos/ProductRecordDto.java
+++ src/main/java/com/example/springEmpresaXYZ/dtos/ProductRecordDto.java
@@ -2,8 +2,17 @@ package com.example.springEmpresaXYZ.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
-public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value, String description) {
-}
+public record ProductRecordDto(
+        @NotBlank
+        @Size(min = 0, max = 250)
+        String name,
+        @NotNull
+        BigDecimal value,
+        @Size(max = 1000)
+        String description,
+        Boolean availability
+) { }
