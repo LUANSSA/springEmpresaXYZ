1745a32973d87c4a5ac39d787066e23399eb9400
Luan Souza <alunoluansenai@gmail.com>
2024-08-25 23:15:31 -0300

    ✨ Novo recurso: Controller de página sobre

---
+++ src/main/java/com/example/springEmpresaXYZ/controllers/Site/PaginaSobreController.java
@@ -0,0 +1,15 @@
+package com.example.springEmpresaXYZ.controllers.Site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginaSobreController {
+
+    @GetMapping("/sobre")
+    public String sobre(Model model) {
+
+        return "sobre";
+    }
+}
