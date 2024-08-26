766672a1f5da4a949275532965aeeadd3f932146
Luan Souza <alunoluansenai@gmail.com>
2024-08-25 23:18:34 -0300

    ✨ Novo recurso: Service de produto

---
+++ src/main/java/com/example/springEmpresaXYZ/services/ProductService.java
@@ -0,0 +1,15 @@
+package com.example.springEmpresaXYZ.services;
+
+import com.example.springEmpresaXYZ.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
